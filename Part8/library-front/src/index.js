5560ab43908abeb6c522ab7620084d7a7818a6d3
Jorge Antonio Balsamo <jorgeb.dev.acc@gmail.com>
2023-12-09 12:03:28 -0300

    Added part 8 (partial)

---
+++ Part8/library-front/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { ApolloProvider, ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+
+const client = ApolloClient({
+  cache: InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000'
+  })
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
