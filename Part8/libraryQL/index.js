69109848c99bdea228cd6981cd1eaca1fecdc2ed
Jorge Antonio Balsamo <jorgeb.dev.acc@gmail.com>
2023-12-10 14:45:21 -0300

    Added part 8 (partial)

--- Part8/libraryQL/index.js
+++ Part8/libraryQL/index.js
@@ -13,102 +13,6 @@ mongoose
   .then(() => console.log("mongodb connected"))
   .catch((err) => console.log(`error connecting: ${err}`));
 
-// let authors = [
-//   {
-//     name: "Robert Martin",
-//     id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-//     born: 1952,
-//   },
-//   {
-//     name: "Martin Fowler",
-//     id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-//     born: 1963,
-//   },
-//   {
-//     name: "Fyodor Dostoevsky",
-//     id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-//     born: 1821,
-//   },
-//   {
-//     name: "Joshua Kerievsky", // birthyear not known
-//     id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-//   },
-//   {
-//     name: "Sandi Metz", // birthyear not known
-//     id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-//   },
-// ];
-
-// /*
-//  * Suomi:
-//  * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
-//  * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
-//  *
-//  * English:
-//  * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
-//  * However, for simplicity, we will store the author's name in connection with the book
-//  *
-//  * Spanish:
-//  * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
-//  * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conección con el libro
-//  */
-
-// let books = [
-//   {
-//     title: "Clean Code",
-//     published: 2008,
-//     author: "Robert Martin",
-//     id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-//     genres: ["refactoring", "agile"],
-//   },
-//   {
-//     title: "Agile software development",
-//     published: 2002,
-//     author: "Robert Martin",
-//     id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-//     genres: ["agile", "patterns", "design"],
-//   },
-//   {
-//     title: "Refactoring, edition 2",
-//     published: 2018,
-//     author: "Martin Fowler",
-//     id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-//     genres: ["refactoring"],
-//   },
-//   {
-//     title: "Refactoring to patterns",
-//     published: 2008,
-//     author: "Joshua Kerievsky",
-//     id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-//     genres: ["refactoring", "patterns"],
-//   },
-//   {
-//     title: "Practical Object-Oriented Design, An Agile Primer Using Ruby",
-//     published: 2012,
-//     author: "Sandi Metz",
-//     id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-//     genres: ["refactoring", "design"],
-//   },
-//   {
-//     title: "Crime and punishment",
-//     published: 1866,
-//     author: "Fyodor Dostoevsky",
-//     id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-//     genres: ["classic", "crime"],
-//   },
-//   {
-//     title: "The Demon ",
-//     published: 1872,
-//     author: "Fyodor Dostoevsky",
-//     id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-//     genres: ["classic", "revolution"],
-//   },
-// ];
-
-/*
-  you can remove the placeholder query once your first one has been implemented 
-*/
-
 let users = [
   {
     username: "Jorge Balsamo",
@@ -131,7 +35,7 @@ const typeDefs = `
   type book {
     title: String!
     published: Int!
-    author: String!
+    author: author!
     id: ID!
     genres: [String!]!
   }
@@ -168,7 +72,7 @@ const resolvers = {
   Query: {
     bookCount: async () => Book.collection.countDocuments(),
     authorCount: async () => Author.collection.countDocuments(),
-    allbooks: async () => Book.find({}),
+    allbooks: async () => Book.find({}).populate('author'),
     allauthors: async () => Author.find({}),
     allusers: () => users,
     me: (_, __, context) => {
@@ -240,9 +144,8 @@ const resolvers = {
 
     login: async (_, args) => {
       let findUser = users.find(
-        (user) =>
-          user.username === args.username && args.password === "somepassword"
-      );
+        user => user.username === args.username && args.password === "somepassword"
+      )
       if (!findUser) return null;
       else {
         return {
@@ -250,9 +153,7 @@ const resolvers = {
             JSON.stringify({
               username: findUser.username,
               password: "somepassword",
-            }),
-            process.env.SECRET
-          ),
+            }), process.env.SECRET ),
         };
       }
     },
